17d2c7df80177f87003e4340d1c2ff0fa62267d5
Julio Developer <juliodeveloper777@gmail.com>
2024-09-17 20:45:07 -0300

    feat: added cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,16 @@ import "express-async-errors";
 import express, { json } from "express";
 import { errorHandler } from "./errors/error-handler";
 import routes from "./routes/index";
+import cors from "cors"
 
 export const app = express();
 
 const startup = async () => {
   app.use(json());
+  app.use(cors());
+  app.use(cors({
+    origin: 'https://www.lowracing.com'
+  }));
   app.use(routes());
   app.use(errorHandler);
 };
